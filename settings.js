52f295c732555aea347c4d8be015e4611e43effb
Alexander Sologub <alexander.sologub@rightscale.com>
2016-03-28 00:17:32 +0300

    Add a blackhole storage module

--- settings.js
+++ settings.js
@@ -1,4 +1,7 @@
 module.exports = {
   // Default storage module
   // storageModule: require('node-red/red/runtime/storage/localfilesystem')
+
+  // Blackhole storage module
+  storageModule: require('./lib/blackhole')
 }
