c7e81de1176a85c1b6735e0b035b0ff54043a465
Alexander Sologub <alexander.sologub@rightscale.com>
2016-03-29 01:37:36 +0300

    Implement an initial version of s3 storage adapter

--- settings.js
+++ settings.js
@@ -1,7 +1,13 @@
 module.exports = {
   // Default storage module
-  // storageModule: require('node-red/red/runtime/storage/localfilesystem')
+  // storageModule: require('node-red/red/runtime/storage/localfilesystem'),
 
   // Blackhole storage module
-  storageModule: require('./lib/blackhole')
-}
+  // storageModule: require('./lib/blackhole'),
+
+  // S3 storage module
+  storageModule: require('./lib/s3'),
+
+  // Unrelated to storage settings
+  uiPort: process.env.VCAP_APP_PORT || 1880
+};
