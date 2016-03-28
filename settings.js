566ea987a8e2fcec8f57b76a5c8f844c8b702619
Alexander Sologub <alexander.sologub@rightscale.com>
2016-03-29 02:34:18 +0300

    Add initial implementation of Redis storage plugin

--- settings.js
+++ settings.js
@@ -6,7 +6,10 @@ module.exports = {
   // storageModule: require('./lib/blackhole'),
 
   // S3 storage module
-  storageModule: require('./lib/s3'),
+  // storageModule: require('./lib/s3'),
+
+  // Redis storage module
+  storageModule: require('./lib/redis'),
 
   // Unrelated to storage settings
   uiPort: process.env.VCAP_APP_PORT || 1880
